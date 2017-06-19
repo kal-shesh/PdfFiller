f71f3c757a18beedc2d9ff787d05ceb7292b89ca
ldoron111 <ldoron111@gmail.com>
2017-06-20 02:12:43 +0300

    Add project files.

---
+++ PdfFiller/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using Newtonsoft.Json.Linq;
+
+namespace PdfFiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(@"C:\Users\lavi\Desktop\Hackaton\testpdf\form.pdf"
+            PdfReader rdr = new PdfReader(args[0]);
+            var dict = JArray.Parse(File.ReadAllText(args[2])).ToObject<KeyValue[]>().ToDictionary(json => json.Key, json => json.Value);
+
+            PdfStamper stamper = new PdfStamper(rdr,new FileStream(args[1],FileMode.Create));
+            foreach (string key in stamper.AcroFields.Fields.Keys)
+            {
+                stamper.AcroFields.SetField(key, dict[key]);
+            }
+            
+            stamper.Close();
+            rdr.Close();
+
+        }
+    }
+}
